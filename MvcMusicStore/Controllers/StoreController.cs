f01571c8e482950cc0011406da17a34c2fcc3399
Julien Plée <julplee@gmail.com>
2013-01-29 12:14:27 +0100

    Adding Store and Home controllers

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,42 @@
+﻿namespace MvcMusicStore.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using MvcMusicStore.Models;
+
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var genres = new List<Genre>
+            {
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+
+            return View(genres);
+        }
+
+        //
+        // GET: /Store/Browse?genre=disco
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+
+        //
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+    }
+}
